14944ec369f14239b9a8bd1b8b303749a7848469
Emre Karabacak <emrekarabacak@Emres-MacBook-Pro.local>
2019-06-21 01:12:01 +0300

    listview implemented, todo items shown on the list

---
+++ lib/screens/todolist.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/model/todo.dart';
+import 'package:todo_app/util/dbhelper.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  DbHelper helper;
+  List<Todo> todos;
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (todos == null) {
+      todos = List<Todo>();
+      getData();
+    }
+
+    return Scaffold(
+      body: todolistItems(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            DbHelper helper = DbHelper();
+            helper.initializeDB().then((result) {
+              Todo todo = Todo(
+                  "Buy oranges", 1, DateTime.now().toString(), "Orangeeedss");
+              helper.inserTodo(todo).then((result) {
+                debugPrint("item added");
+              });
+            });
+          },
+          tooltip: "Add new todo",
+          child: Icon(Icons.add)),
+    );
+  }
+
+  ListView todolistItems() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Text(this.todos[position].priority.toString()),
+            ),
+            title: Text(this.todos[position].title),
+            subtitle: Text(this.todos[position].date),
+            onTap: () {
+              debugPrint("Tapped on " + this.todos[position].id.toString());
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  void getData() {
+    helper = DbHelper();
+    final dbFuture = helper.initializeDB();
+    dbFuture.then((result) {
+      final todosFuture = helper.getTodos();
+      todosFuture.then((result) {
+        List<Todo> todoList = List<Todo>();
+        count = result.length;
+        for (int i = 0; i < count; i++) {
+          todoList.add(Todo.fromObject(result[i]));
+          debugPrint(todoList[i].title);
+        }
+        setState(() {
+          todos = todoList;
+          count = count;
+        });
+        debugPrint("Items " + count.toString());
+      });
+    });
+  }
+}
